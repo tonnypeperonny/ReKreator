a0e5941206c1812c5dd0a9c4cfe3a24b0ce3c2ac
Raman But-Husaim <raman_but-husaim@live.com>
2018-06-11 22:21:58 +0300

    initial commit

---
+++ src/ReKreator.Utils.Tests/PreconditionTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using FluentAssertions;
+
+using NUnit.Framework;
+
+namespace ReKreator.Utils.Tests
+{
+    [TestFixture]
+    public sealed class PreconditionTests
+    {
+        [Test]
+        public void CheckNotNull_WhenTheNulLValueIsPassed_ShouldThrowException()
+        {
+            // arrange
+            string value = null;
+            
+            // act
+            Action act = () => Preconditions.CheckNotNull(value, nameof(value));
+            
+            // assert
+            act.Should().ThrowExactly<ArgumentNullException>();
+        }
+    }
+}
