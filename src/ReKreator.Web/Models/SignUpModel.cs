abe00421b617f870a0d92e36db7f0b9a65ac5fc4
Egor Makarov <egormakarow@gmail.com>
2018-07-04 11:08:07 +0300

    #35[ReWork] Address styling issues on the landing page

--- src/ReKreator.Web/Models/SignUpModel.cs
+++ src/ReKreator.Web/Models/SignUpModel.cs
@@ -5,7 +5,6 @@ namespace ReKreator.Web.Models
     public class SignUpModel
     {
         [Required]
-        [DataType(DataType.EmailAddress)]
         [MaxLength(250)]
         [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", 
             ErrorMessageResourceName = "EmailValidator",
