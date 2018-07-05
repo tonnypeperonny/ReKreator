8ff18e4c6d31c51402d5aa525b05349ef128f11a
Egor Makarov <egormakarow@gmail.com>
2018-07-05 13:45:46 +0300

    #18 Global exception handling and error page

---
+++ src/ReKreator.Web/Controllers/ErrorController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ReKreator.Web.Models;
+
+namespace ReKreator.Web.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [HttpGet]
+        public ActionResult DisplayHttpError(HttpStatusCode? id)
+        {
+            var httpStatusCode = (int)(id ?? HttpStatusCode.InternalServerError);
+            Response.StatusCode = httpStatusCode;
+
+            var error = new ErrorModel
+            {
+                Message = Resources.Resources.ResourceManager.GetString($"HttpError{httpStatusCode}Message"),
+                ErrorTitle = Resources.Resources.ResourceManager.GetString($"HttpError{httpStatusCode}Title")
+            };
+
+            return View("DisplayHttpError",error);
+        }
+    }
+}
